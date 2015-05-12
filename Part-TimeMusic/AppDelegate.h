ba326bf949ce247cde13fd17c2dfa96e9ba10ba4
牛超凡 <niuchaofan@MacBook-Air.local>
2015-05-12 12:02:11 +0800

    Initial Commit

---
+++ Part-TimeMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Part-TimeMusic
+//
+//  Created by 牛超凡 on 15/5/12.
+//  Copyright (c) 2015年 litterBlck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Part-TimeMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Part-TimeMusic
+//
+//  Created by 牛超凡 on 15/5/12.
+//  Copyright (c) 2015年 litterBlck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
